0ededdd660e80a92ff9f8c1b14a7ea70750d59e2
AbuHurairaRaheem <ahuraira784@gmail.com>
2025-07-12 00:03:53 +0500

    Add index2.html and script2.js for Assignment 2

---
+++ Assigment 1 (01-20)/script.js
@@ -0,0 +1,24 @@
+// /*2 */
+
+// let greeating ="hello,user"
+//  alert(greeating)
+
+// /*3 */
+// let usurage= 20
+// let futureage= usurage+5;
+// console.log(futureage)
+
+// 4
+// let usurname=absd 
+// let $prise 
+// let _total$prise
+// // let=let let is a reverse keyword
+// let 1sed=
+// // cannot start with number,
+// // hypen should not be allowed use camael case.
+
+// 5
+let a =8;
+let b =2;
+
+
